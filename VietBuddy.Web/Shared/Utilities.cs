6d4404acf839b180c0d7a6e7c444cc9224c08d80
Ben Ghaner <bghaner@outlook.com>
2021-05-22 21:55:36 -0400

    Refactor to simplify

---
+++ VietBuddy.Web/Shared/Utilities.cs
@@ -0,0 +1,14 @@
+using System.Text.Json;
+
+namespace VietBuddy.Web.Shared
+{
+    public interface IJsonClonable {}
+    public static class JsonExtensions
+    {
+        public static T Clone<T>(this T original) where T : IJsonClonable
+        {
+            string json = JsonSerializer.Serialize<T>(original);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
