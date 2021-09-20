f0c5cb6c79cb252e32d0a700f2fcc5de58cba72a
Ben Ghaner <benghaner@gmail.com>
2021-09-19 21:22:33 -0400

    Load sample data on app startup

--- VietBuddy.Demo/Program.cs
+++ VietBuddy.Demo/Program.cs
@@ -20,7 +20,7 @@ namespace VietBuddy.Demo
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredModal();
             builder.Services.AddScoped<ITranslationRepository, TranslationRepository>();
 
